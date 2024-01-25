e8a6cf3cac106af29e3d444ae50d8a2bf483d60d
griffin-webster <griffin.webster@students.fhu.edu>
2024-01-25 16:40:05 -0600

    Initial main.py commit
    I have filled out the functions with working code.

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
-def sum_of_products(list1, list2):
+import operator
+
+int1 = int(input("Give a list of numbers:"))
+int2 = int(input("Give a second list of numbers (the same length as the first):"))
+
+str1 = str(int1)
+str2 = str(int2)
+
+if len(str1) < len(str2) or len(str1) > len(str2):
+    print("Error")
+else:
     pass
+
+
+def sum_of_products(list1, list2):
+    result = list(map(operator.mul, list1, list2))
+    return sum(result)
+    
+
+
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
-    pass
+    list1 = list(map(int, str(int1)) )
+    list2 = list(map(int, str(int2)) )
+    sum_of_products(list1, list2)
+
+print(sum_of_products(list1, list2))
