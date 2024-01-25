35e014343969d80e8e9abb2e92b2750d62e422d7
griffin-webster <griffin.webster@students.fhu.edu>
2024-01-25 16:46:24 -0600

    Update main.py
    Housed the rest of my code in the bottom function.

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
-import operator
 
-int1 = int(input("Give a list of numbers:"))
-int2 = int(input("Give a second list of numbers (the same length as the first):"))
-
-str1 = str(int1)
-str2 = str(int2)
-
-if len(str1) < len(str2) or len(str1) > len(str2):
-    print("Error")
-else:
-    pass
-
+import operator 
 
 def sum_of_products(list1, list2):
     result = list(map(operator.mul, list1, list2))
     return sum(result)
-    
-
-
 
 if __name__ == '__main__':
    #REMOVE PASS AND YOUR CODE GOES HERE
+    int1 = int(input("Give a list of numbers:"))
+    int2 = int(input("Give a second list of numbers (the same length as the first):"))
+    str1 = str(int1)
+    str2 = str(int2)
+    if len(str1) < len(str2) or len(str1) > len(str2):
+        print("Error")
+    else:
+        pass
     list1 = list(map(int, str(int1)) )
     list2 = list(map(int, str(int2)) )
     sum_of_products(list1, list2)
